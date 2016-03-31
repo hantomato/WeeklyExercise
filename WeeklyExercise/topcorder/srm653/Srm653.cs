5226621838d7acf100890691ae79bdd628f532d2
Noh MoonJin <moonjin@gmail.com>
2016-03-31 10:47:46 +0900

    111

--- WeeklyExercise/topcorder/srm653/Srm653.cs
+++ WeeklyExercise/topcorder/srm653/Srm653.cs
@@ -15,6 +15,7 @@ namespace WeeklyExercise.topcorder.srm652
         // ex) 2*4 + 1*2 + 2*4 + 3*5 + 1*2 = 35
         public int solve(int[] a)
         {
+
             //
             return 0;
         }
