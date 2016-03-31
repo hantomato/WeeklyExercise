8d90341898e44f1a7b548a35155c8f1d6117738a
Noh MoonJin <moonjin@gmail.com>
2016-03-31 10:33:17 +0900

    test

---
+++ WeeklyExercise/topcorder/srm653/Srm653.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// https://community.topcoder.com/stat?c=problem_statement&pm=13678
+/// </summary>
+namespace WeeklyExercise.topcorder.srm652
+{
+    class CountryGroup
+    {
+        // ex) 2*4 + 1*2 + 2*4 + 3*5 + 1*2 = 35
+        public int solve(int[] a)
+        {
+            
+        }
+
+        public void doing()
+        {
+            print(solve(new int[] { 2, 2, 3, 3, 3 }));
+            print(solve(new int[] { 1, 1, 1, 1, 1 }));
+            print(solve(new int[] { 3, 3 }));
+            print(solve(new int[] { 4, 4, 4, 4, 1, 1, 2, 2, 3, 3, 3 }));
+            print(solve(new int[] { 2, 1, 2, 2, 1, 2 }));
+        }
+
+        public void print(int text)
+        {
+            Logger.log("return: " + text);
+        }
+    }
+
+}
